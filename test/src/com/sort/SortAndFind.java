26520b2928986a1312bf2865ded9cf2d3fb96029
ListenYoung <502961269@qq.com>
2017-10-24 14:49:31 +0800

    complete sort

--- test/src/com/sort/SortAndFind.java
+++ test/src/com/sort/SortAndFind.java
@@ -4,6 +4,14 @@ package com.sort;
  * Created by ListenYoung on 16/8/13.
  */
 public class SortAndFind {
+    /**
+     * 递归二分排序
+     * @param arr
+     * @param from
+     * @param to
+     * @param key
+     * @return log(n)
+     */
     public int binarySearch(int[] arr , int from , int to , int key){
         if(from < 0 || to < 0 || from > to){
             return -1;
@@ -23,6 +31,14 @@ public class SortAndFind {
         return binarySearch(arr , from , to , key);
     }
 
+    /**
+     * 循环二分排序
+     * @param arr
+     * @param fromIndex
+     * @param toIndex
+     * @param key
+     * @return log(n)
+     */
     public int binarySearchLoop(int arr[] , int fromIndex , int toIndex , int key){
         int low = fromIndex;
         int high = toIndex - 1;
@@ -41,6 +57,103 @@ public class SortAndFind {
         return -(low + 1);
     }
 
+    /**
+     * 直接插入排序
+     * @param arr
+     * n^2
+     */
+    public void insertSort(int[] arr){
+        int n = arr.length;
+        //第一个元素一定是在正确位置
+        for (int i = 1 ; i < n ; i ++) {
+            int j = i;
+            int target = arr[i];//这个是要被插入的元素
+            //确定要插入的位置
+            while (j > 0 && target < arr[j - 1]){
+                arr[j] = arr[j - 1]; //向后移动，为要插入的元素腾个地方
+                j --;
+            }
+            arr[j] = target;
+        }
+    }
+
+    /**
+     * 选择排序
+     * @param arr
+     * n^2
+     */
+    public void selectionSort(int[] arr){
+        for (int i = 0 ; i < arr.length - 1 ; i ++){
+            int location = i;
+            for (int j = i + 1 ; j < arr.length ; j ++){
+                if (arr[j] < arr[location]){
+                    location = j;
+                }
+            }
+            if (location != i){
+                int temp = arr[i];
+                arr[i] = arr[location];
+                arr[location] = temp;
+            }
+        }
+    }
+
+    /**
+     * 真·晓旭亲传 优化冒泡排序
+     * @param arr
+     * n^2
+     */
+    public void bubbleSort(int[] arr){
+        for (int i = 0 ; i < arr.length - 1 ; i ++){
+            boolean flag = false; // 标识变量 防止无用循环 晓旭亲传
+            for (int j = 0 ; j < arr.length - 1 - i ; j ++){
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag = true;
+                }
+            }
+            if (flag){
+                flag  = false;
+            }else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 希尔排序
+     * @param arr
+     * nlog(n) n^2
+     */
+    public void shellSort(int[] arr){
+        //确定shell每一次排序之间的间隔
+        for (int increment = arr.length / 2 ; increment > 0 ; increment /= 2){
+            //对每一轮间隔之间的子数组进行插入排序
+            for (int i = increment ; i < arr.length ; i ++){
+                int temp = arr[i];//要插入的元素
+                int j = 0;
+                for (j = i - increment ; j >= 0 ; j --){
+                    if (temp < arr[j]){
+                        arr[j + increment] = arr[j];//说明要插入到j的位置，j要往后稍一稍
+                    }else{
+                        break;
+                    }
+                }
+                arr[j + increment] = temp;//因为又进了一次循环，break出来的 所以要还原位置
+            }
+        }
+    }
+
+    /**
+     * 快速排序 填坑式，效率更高
+     * @param arr
+     * @param start
+     * @param end
+     * nlog(n) n^2
+     * 由于每次递归都要分配一个额外空间，故有log(n)的空间复杂度
+     */
     public void quickSort(int[] arr , int start , int end){
         if(start >= end){
             return;
@@ -70,15 +183,21 @@ public class SortAndFind {
         quickSort(arr , first + 1 , last);
     }
 
-    public int[] mergeSort(int[] arr , int start , int end){
+    /**
+     * 归并排序
+     * @param arr
+     * @param start
+     * @param end
+     * @return t:nlog(n)  s:n + log(n)
+     */
+    public void mergeSort(int[] arr , int start , int end){
         if(start > end){
-            return null;
+            return;
         }
         int mid = (start + end) >>> 1;
         mergeSort(arr , start , mid);
         mergeSort(arr , mid + 1 , end);
         merge(arr , start , mid , end);
-        return arr;
     }
 
     public void merge(int[] arr , int start , int mid , int end){
@@ -105,7 +224,13 @@ public class SortAndFind {
         }
     }
 
-    //http://blog.csdn.net/apei830/article/details/6584645
+    /**
+     * 堆排序
+     * http://blog.csdn.net/apei830/article/details/6584645
+     * @param arr
+     * s:1
+     * 建堆 n ； 调整堆 log(n) ； 一共n-1次 -- t:nlog(n)
+     */
     public void heapSort(int[] arr){
         for(int i = 0 ; i < arr.length ; i ++){
             createMaxHeap(arr , arr.length - 1 - i);
@@ -117,16 +242,17 @@ public class SortAndFind {
 
     public void createMaxHeap(int[] arr , int lastIndex){
         for(int i = (lastIndex - 1) / 2 ; i >= 0 ; i --){
+            //这样算出来的i是堆中第一个非叶结点
             int k = i;
             while (2 * k + 1 < lastIndex){
-                int biggerIndex = 2 * k + 1;
+                int biggerIndex = 2 * k + 1;//记录子节点中最大的那个节点的位置
                 //判断是否有右节点
                 if(biggerIndex < lastIndex){
                     if(arr[biggerIndex] < arr[biggerIndex + 1]){
                         biggerIndex ++ ;
                     }
                 }
-
+                //如果子节点比父节点大 就交换 重建下面的堆
                 if(arr[k] < arr[biggerIndex]){
                     int temp = arr[k];
                     arr[k] = arr[biggerIndex];
@@ -140,5 +266,7 @@ public class SortAndFind {
         }
     }
 
-    //todo 冒泡 插入 选择 基数 希尔
+    /*
+     基数排序：排序算法的应用，传说中的多关键字排序。比较适合桶排序——第一关键字一样的一桶，比较第二关键字；以此类推。
+     */
 }
